d880ff6974c4ac5cd9cb80cedde74950ded0311e
Abigai1h09 <abigailh49@nycstudents.net>
2025-12-21 00:45:06 +0000

    encryption.project

--- EncrytionProject/Main.java
+++ EncrytionProject/Main.java
@@ -24,13 +24,14 @@ class Main {
 
     //  DECODE 
 
-    String file2 = Input.readFile("Encode3.txt");
-    String decoded = morseDecrypt(
-                        asciiDecode(
-                          brailleDecode(file2)
-                        )
-                     );
-    Input.writeFile("Decode.txt", decoded); 
+  String file2 = Input.readFile("Encode3.txt");
+  String decoded1 = morseDecrypt(
+     asciiDecode(
+     brailleDecode(encoded3)
+      )
+    );
+
+    Input.writeFile("Decode1.txt", decoded1); 
     String decoded3 = brailleDecode(file2);
     Input.writeFile("Decode3.txt", decoded3);
 
@@ -44,14 +45,14 @@ class Main {
     for(int i = 0; i < txt.length(); i++){
       build.append((int)txt.charAt(i)).append(" ");
     }
-    return build.toString();
+    return build.toString(); //takes everything to go back to ascii//
   }
 
   String asciiDecode(String txt){
     StringBuilder build = new StringBuilder();
     String[] parts = txt.split(" ");
     for(String p : parts){
-      if(!p.isEmpty())
+      if(!p.isEmpty()) // trying to not skip strings
         build.append((char)Integer.parseInt(p));
     }
     return build.toString();
@@ -59,36 +60,42 @@ class Main {
 
 // LEVEL 3 BRAILLE 
 String brailleEncode(String txt){
-StringBuilder build = new StringBuilder();
-for(int i = 0; i < txt.length(); i++){
-char c = txt.charAt(i);
-if(c == '\n'){
-build.append('\n');
-} else {
-build.append((char)(0x2800 + c));
-}
-}
-return build.toString();
+  StringBuilder build = new StringBuilder();
+    for(int i = 0; i < txt.length(); i++){
+      char c = txt.charAt(i);
+
+      if(c == ' ' || c == '\n'){
+        build.append(c); // preserve delimiters
+      } else {
+        build.append((char)(0x2800 + c));
+      }
+  }
+    return build.toString();
 }
 
+
 String brailleDecode(String txt){
-StringBuilder build = new StringBuilder();
-for(int i = 0; i < txt.length(); i++){
-char c = txt.charAt(i);
-if(c == '\n'){
-build.append('\n');
-} else {
-build.append((char)(c - 0x2800));
-}
-}
-return build.toString();
+  StringBuilder build = new StringBuilder();
+    for(int i = 0; i < txt.length(); i++){
+      char c = txt.charAt(i);
+
+      if(c == ' ' || c == '\n'){
+        build.append(c);
+    } else {
+      build.append((char)(c - 0x2800));
+        }
+    }
+  return build.toString();
 }
 
   //  LEVEL 1 MORSE 
   String[] morse = {
     ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---",
     "-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-",
-    "..-","...-",".--","-..-","-.--","--.."
+    "..-","...-",".--","-..-","-.--","--..","-----",".----","..---",
+    "...--","....-",".....","-....","--...","---..","----.",".-.-.-",
+    "--..--","..--..",".----.","-.-.--","-..-.","-.--.","-.--.-",".-...",
+    "---...","-.-.-.","-...-","-....-",".-.-.","..--.-",".-..-.","...-..-", ".--.-."
   };
 
   String morseEncrypt(String txt){
@@ -104,8 +111,11 @@ return build.toString();
       else if(ch == ' '){
         build.append("/ ");
       }
-      else{
+      else if(ch == ' '){
         build.append("# ");
+      }
+      else{
+        // skip character
       }
     }
     return build.toString();
